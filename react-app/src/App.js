dedcfa6d8056e4072c6caccb53c1f4443e5a973d
yihimin <yihimin01@gmail.com>
2025-02-09 00:30:09 +0900

    states

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,16 +1,40 @@
 import "./App.css";
+import React, { Component } from "react";
 import Subject from "./components/Subject";
 import TOC from "./components/TOC";
 import Content from "./components/Content";
 
-function App() {
-  return (
-    <div className="App">
-      <Subject title="WEB" sub="world wide web!"></Subject>
-      <TOC></TOC>
-      <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: { title: "WEB", sub: "world wide web!" },
+      contents: [
+        { id: 1, title: "HTML", desc: "HTML is HyperText..." },
+        { id: 2, title: "CSS", desc: "CSS is for design..." },
+        {
+          id: 3,
+          title: "JavaScript",
+          desc: "JavaScript is for interactive...",
+        },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+        ></Subject>
+        <TOC data={this.state.contents}></TOC>
+        <Content
+          title="HTML"
+          desc="HTML is HyperText Markup Language."
+        ></Content>
+      </div>
+    );
+  }
 }
 
 export default App;
