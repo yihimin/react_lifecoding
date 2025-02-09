35792076ac8a40d868e4be1a1fb109c17e5080ae
yihimin <yihimin01@gmail.com>
2025-02-09 18:19:07 +0900

    16강

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      mode: "welcome",
       subject: { title: "WEB", sub: "world wide web!" },
+      welcome: { title: "Welcome", desc: "Hello, React!!" },
       contents: [
         { id: 1, title: "HTML", desc: "HTML is HyperText..." },
         { id: 2, title: "CSS", desc: "CSS is for design..." },
@@ -21,6 +23,14 @@ class App extends Component {
     };
   }
   render() {
+    let _title, _desc;
+    if (this.state.mode === "welcome") {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === "read") {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App">
         <Subject
@@ -28,10 +38,7 @@ class App extends Component {
           sub={this.state.subject.sub}
         ></Subject>
         <TOC data={this.state.contents}></TOC>
-        <Content
-          title="HTML"
-          desc="HTML is HyperText Markup Language."
-        ></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
